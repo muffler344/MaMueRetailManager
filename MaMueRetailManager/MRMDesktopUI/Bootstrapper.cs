5defc0e5d7058ba3b0c8aa51c67292d00f99e41a
marvin.mueller <marvin.mueller@ARALAP025>
2020-10-08 11:27:48 +0200

    Added LoginView

--- MaMueRetailManager/MRMDesktopUI/Bootstrapper.cs
+++ MaMueRetailManager/MRMDesktopUI/Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using MRMDesktopUI.Helpers;
 using MRMDesktopUI.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace MRMDesktopUI
 {
@@ -16,6 +18,11 @@ namespace MRMDesktopUI
         public Bootstrapper()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+                PasswordBoxHelper.BoundPasswordProperty,
+                "Password",
+                "PasswordChanged");
         }
 
         protected override void Configure()
