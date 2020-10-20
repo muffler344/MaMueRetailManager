97fab09886c8c0b4f3e0d0b22e79d12655918c45
marvin.mueller <marvin.mueller@ARALAP025>
2020-10-20 09:26:08 +0200

    Fixed Bug in ShellView

--- MaMueRetailManager/MRMDesktopUI/ViewModels/ShellViewModel.cs
+++ MaMueRetailManager/MRMDesktopUI/ViewModels/ShellViewModel.cs
@@ -2,10 +2,6 @@
 using MRMDesktopUI.EventModels;
 using MRMDesktopUI.Library.Api;
 using MRMDesktopUI.Library.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -14,17 +10,14 @@ namespace MRMDesktopUI.ViewModels
     public class ShellViewModel : Conductor<object>, IHandle<LogOnEvent>
     {
         private IEventAggregator _events;
-        private SalesViewModel _salesVM;
         private ILoggedInUserModel _user;
         private IAPIHelper _apiHelper;
         public ShellViewModel(
             IEventAggregator events, 
-            SalesViewModel salesVM, 
             ILoggedInUserModel user,
             IAPIHelper apiHelper)
         {
             _events = events;
-            _salesVM = salesVM;
             _user = user;
             _apiHelper = apiHelper;
 
@@ -68,7 +61,7 @@ namespace MRMDesktopUI.ViewModels
 
         public async Task HandleAsync(LogOnEvent message, CancellationToken cancellationToken)
         {
-            await ActivateItemAsync(_salesVM, cancellationToken);
+            await ActivateItemAsync(IoC.Get<SalesViewModel>(), cancellationToken);
             NotifyOfPropertyChange(() => IsLoggedIn);
         }
     }
